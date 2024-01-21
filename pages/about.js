5a8b1134422cf8ef245d64e98d59e0eb971a7c49
KAMRUZZAMAN LEION <leion.kamruzzaman@brilliant.com.bd>
2024-01-21 20:11:43 +0600

    added testimonial component

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,23 @@ import Page from "../components/page";
 export default function About() {
   return (
     <Page>
-      <h1>About</h1>
+      <div class="shrink-0 w-[500px] grid grid-cols-[7rem,_1fr] rounded-lg overflow-hidden relative">
+        <img
+          src="/imgs/head-shots/16.jpg"
+          alt="Testimonial image for Matthew I."
+          class="w-full h-44 object-cover"
+        />
+        <div class="bg-slate-900 text-slate-50 p-4">
+          <span class="block font-semibold text-lg mb-1">Matthew I.</span>
+          <span class="block mb-3 text-sm font-medium">Founder of XYZ</span>
+          <span class="block text-sm text-slate-300">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+            nostrum labore dolor facilis, nesciunt facere mollitia nam
+            aspernatur esse corporis!
+          </span>
+        </div>
+        <span class="text-7xl absolute top-2 right-2 text-slate-700"></span>
+      </div>
     </Page>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -3,14 +3,20 @@ import Page from "../components/page";
 export default function Contact() {
   return (
     <Page>
-      {/* <h1>Contact</h1> */}
       <div
         class="w-full relative overflow-hidden overflow-y-scroll no-scrollbar bg-white border-[1px] border-slate-300"
         style={{ display: "block" }}
       >
         <section
           class="px-4 py-12 bg-violet-600"
-          // style='background-image: url("https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=1734&amp;q=80"); background-size: cover; background-position: center center;'
+          style={{
+            // backgroundImage: url(
+            //   "https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=1734&amp;q=80"
+            // ),
+            backgroundImage: `url(https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=1734&amp;q=80)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+          }}
         >
           <div class="h-96 bg-slate-950/70 backdrop-blur rounded-lg w-full max-w-3xl mx-auto overflow-y-scroll shadow-xl cursor-text font-mono">
             <div class="w-full p-3 bg-slate-900 flex items-center gap-1 sticky top-0">
@@ -22,7 +28,7 @@ export default function Contact() {
               </span>
             </div>
             <div class="p-2 text-slate-100 text-lg">
-              <p>Hey there! We are excited to link 🔗</p>
+              <p>Hey there! Were excited to link 🔗</p>
               <p class="whitespace-nowrap overflow-hidden font-light">
                 ------------------------------------------------------------------------
               </p>
@@ -30,19 +36,87 @@ export default function Contact() {
                 To start, could you give us{" "}
                 <span class="text-violet-300">your email?</span>
               </p>
-              <form>
-                <input
-                  type="text"
-                  class="sr-only"
-                  autocomplete="off"
-                  value=""
-                />
-              </form>
-              <p>
-                <span class="text-emerald-400">➜</span>{" "}
-                <span class="text-cyan-300">~</span>{" "}
-                <span class="opacity-50">Enter email: </span>
+              <p class="text-emerald-300">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="inline-block mr-2"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+                <span>sf</span>
               </p>
+              <p>
+                Awesome! And whats{" "}
+                <span class="text-violet-300">your name?</span>
+              </p>
+              <p class="text-emerald-300">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="inline-block mr-2"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+                <span>sfsdf</span>
+              </p>
+              <p>
+                Perfect, and{" "}
+                <span class="text-violet-300">how can we help you?</span>
+              </p>
+              <p class="text-emerald-300">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="inline-block mr-2"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                  <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                </svg>
+                <span>dsds</span>
+              </p>
+              <p>Beautiful! Heres what weve got:</p>
+              <p>
+                <span class="text-blue-300">email:</span> sf
+              </p>
+              <p>
+                <span class="text-blue-300">name:</span> sfsdf
+              </p>
+              <p>
+                <span class="text-blue-300">description:</span> dsds
+              </p>
+              <p>Look good?</p>
+              <div class="flex gap-2 mt-2">
+                <button class="px-3 py-1 text-base hover:opacity-90 transition-opacity rounded bg-slate-100 text-black">
+                  Restart
+                </button>
+                <button class="px-3 py-1 text-base hover:opacity-90 transition-opacity rounded bg-indigo-500 text-white">
+                  Send it!
+                </button>
+              </div>
             </div>
           </div>
         </section>
